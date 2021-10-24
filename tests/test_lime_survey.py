fb8e5874c4df68f85a284d4db149d83c1ac17563
Dawei Fu <davidfuswe@gmail.com>
2021-10-24 21:27:05 +0200

    Implemented tests for read_response method. Updated read_response method based on tests.

--- tests/test_lime_survey.py
+++ tests/test_lime_survey.py
@@ -7,11 +7,13 @@ from pandas._testing import assert_frame_equal
 
 from n2survey.lime import LimeSurvey, read_lime_questionnaire_structure
 
+structure_file = "data/survey_structure_2021.xml"
+response_file = "data/dummy_data_2021_codeonly.csv"
+
 
 class TestLimeSurveyInitialisation(unittest.TestCase):
     """Test LimeSurvey class initialisation"""
 
-    structure_file = "data/survey_structure_2021.xml"
     default_plot_options = {
         "cmap": "Blues",
         "output_folder": os.path.abspath(os.curdir),
@@ -32,13 +34,22 @@ class TestLimeSurveyInitialisation(unittest.TestCase):
         """Test simple initialisation"""
 
         survey = LimeSurvey(
-            structure_file=self.structure_file,
+            structure_file=structure_file,
         )
 
-        structure_dict = read_lime_questionnaire_structure(self.structure_file)
+        structure_dict = read_lime_questionnaire_structure(structure_file)
         section_df = pd.DataFrame(structure_dict["sections"])
         section_df = section_df.set_index("id")
         question_df = pd.DataFrame(structure_dict["questions"])
+        for i in range(question_df.shape[0]):
+            if question_df.loc[i]["format"] is None:
+                if "Y" in question_df.loc[i]["choices"].keys():
+                    question_df.loc[i, ["format"]] = "multiple_choice"
+                elif "SQ" in question_df.loc[i]["name"]:
+                    question_df.loc[i, ["format"]] = "array"
+                else:
+                    question_df.loc[i, ["format"]] = "single_choice"
+        question_df["name"] = question_df["name"].str.replace("T", "_other")
         question_df = question_df.set_index("name")
 
         self.assertEqual(
@@ -63,7 +74,7 @@ class TestLimeSurveyInitialisation(unittest.TestCase):
 
         cmap = "Reds"
         survey = LimeSurvey(
-            structure_file=self.structure_file,
+            structure_file=structure_file,
             cmap=cmap,
         )
 
@@ -84,7 +95,7 @@ class TestLimeSurveyInitialisation(unittest.TestCase):
 
         output_dir = "plot/"
         survey = LimeSurvey(
-            structure_file=self.structure_file,
+            structure_file=structure_file,
             output_folder=output_dir,
         )
 
@@ -101,13 +112,104 @@ class TestLimeSurveyInitialisation(unittest.TestCase):
         """Test initialisation with default figsize option for plotting"""
 
         figsize = (10, 15)
-        survey = LimeSurvey(structure_file=self.structure_file, figsize=figsize)
+        survey = LimeSurvey(structure_file=structure_file, figsize=figsize)
 
         self.assertEqual(survey.plot_options["figsize"], figsize)
         self.assertEqual(survey.plot_options["cmap"], self.default_plot_options["cmap"])
         self.assertEqual(
             survey.plot_options["output_folder"],
             self.default_plot_options["output_folder"],
+        )
+
+
+class TestLimeSurveyReadResponse(unittest.TestCase):
+    """Test LimeSurvey reading response"""
+
+    def test_read_response(self):
+        """Test reading response from dummy data csv"""
+
+        survey = LimeSurvey(structure_file=structure_file)
+        survey.read_response(responses_file=response_file)
+
+        first_question = survey.response.columns.get_loc("A00")
+        last_question = survey.response.columns.get_loc("M1")
+        missing_columns = [
+            column
+            for column in survey.response.columns[first_question:last_question]
+            if column not in survey.questions.index and "other" not in column
+        ]
+
+        self.assertEqual(bool(missing_columns), False)
+
+    def test_single_choice_dtype(self):
+        """Test data for single choice questions is unordered categorical dtype"""
+
+        survey = LimeSurvey(structure_file=structure_file)
+        survey.read_response(responses_file=response_file)
+
+        single_choice_questions = [
+            question
+            for question in survey.questions.index
+            if survey.questions.loc[question]["format"] == "single_choice"
+        ]
+
+        self.assertEqual(
+            (survey.response[single_choice_questions].dtypes == "category").all(),
+            True,
+        )
+        self.assertEqual(
+            bool(
+                [
+                    column
+                    for column in single_choice_questions
+                    if survey.response[column].cat.ordered
+                ]
+            ),
+            False,
+        )
+
+    def test_array_dtype(self):
+        """Test data for array questions is ordered categorical dtype"""
+
+        survey = LimeSurvey(structure_file=structure_file)
+        survey.read_response(responses_file=response_file)
+
+        array_questions = [
+            question
+            for question in survey.questions.index
+            if survey.questions.loc[question]["format"] == "array"
+        ]
+
+        self.assertEqual(
+            (survey.response[array_questions].dtypes == "category").all(),
+            True,
+        )
+        self.assertEqual(
+            bool(
+                [
+                    column
+                    for column in array_questions
+                    if not survey.response[column].cat.ordered
+                ]
+            ),
+            False,
+        )
+
+    def test_multiple_choice_dtype(self):
+        """Test data for multiple choice questions is bool dtype"""
+
+        survey = LimeSurvey(structure_file=structure_file)
+        survey.read_response(responses_file=response_file)
+
+        multiple_choice_questions = [
+            question
+            for question in survey.questions.index
+            if survey.questions.loc[question]["format"] == "multiple_choice"
+        ]
+
+        self.assertEqual(
+            (survey.response[multiple_choice_questions].dtypes == "bool").all(),
+            True,
         )
 
 
