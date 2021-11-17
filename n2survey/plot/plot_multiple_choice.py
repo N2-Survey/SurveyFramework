88fc62c2c20e2a17a1bd153185789f155fd5d64f
Dawei Fu <davidfuswe@gmail.com>
2021-11-17 12:53:13 +0100

    Updated plot_multiple_choice function. Created test. Updated common.py with read_responses step.

--- n2survey/plot/plot_multiple_choice.py
+++ n2survey/plot/plot_multiple_choice.py
@@ -1,114 +1,25 @@
 from textwrap import wrap
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
-
-from n2survey import lime
-
-
-def load_raw_data(path_to_data, question_id):
-    """
-    Loads raw data csv file into dataframe
-
-    Args:
-        path_to_data (str): Path to raw data csv file
-        question_id (str): Code/id of the question to plot
-
-    Returns:
-        dataframe: A dataframe containing raw data for response to specified question
-    """
-
-    # Read csv into dataframe
-    raw_data = pd.read_csv(path_to_data)
-
-    # Select columns containing response to specified question
-    columns = [
-        column
-        for column in raw_data.columns
-        if column[: len(question_id) + 1] == question_id + "["
-    ]
-
-    return raw_data[columns]
-
-
-def preprocess_data(data_df, display_threshold):
-    """
-    Preprocesses data
-
-    Args:
-        data_df (Dataframe): Dataframe containing response data
-        display_threshold (float): Percentage threshold of the category to be included in the plot
-
-    Returns:
-        int: Total number of responses
-        dataframe: A dataframe containing processed data
-    """
-
-    # Process data
-    response_raw = data_df.notnull()
-
-    # Create new 'Percentage' column
-    response_counts = pd.DataFrame(response_raw.sum(axis=0))
-    response_counts = response_counts.rename(columns={0: "counts"})
-    total_response = response_raw.shape[0]
-    response_counts["percentage"] = response_counts["counts"] / total_response * 100
-
-    # Filter out categories below threshold
-    response_to_plot = response_counts[
-        response_counts["percentage"] >= display_threshold
-    ].sort_values(by="percentage", ascending=False)
-
-    return total_response, response_to_plot
-
-
-def extract_question_info(path_to_structure, question_id):
-    """
-    Extracts texts for question and response options
-
-    Args:
-        path_to_structure (str): path to survey structure xml file
-        question_id (str): Code/id of the question to plot
-
-    Returns:
-        dict: dictionary containing texts for question title and response categories
-    """
-
-    # Parse xml file into dictionary
-    structure_dict = lime.read_lime_questionnaire_structure(path_to_structure)
-
-    # Extract texts for specified question
-    text_dict = {}
-    for question in structure_dict["questions"]:
-        if question["question_group"] == question_id:
-            if question["name"][-5:] != "other":
-                text_dict["question_text"] = question["label"]
-                text_dict[question["name"].replace("_", "[") + "]"] = question[
-                    "choices"
-                ]["Y"]
-                text_dict[question["name"].replace("T", "[other]")] = "Other"
-
-    return text_dict
 
 
 def create_bar_plot(
     data_df,
-    text_dict,
-    total_response,
+    question_label,
+    tick_labels,
     color_scheme,
     display_title,
     fig_dim,
     bar_thickness,
     bar_spacing,
-    wrap_text,
 ):
     """
-    Creates bar plot
+    Create bar plot
 
     Args:
         data_df (dataframe): Dataframe containing data to be plotted
-        text_dict (dict): Dictionary containing texts for question and response categories
-        total_reponse (int): Total number of responses
+        question_label (str): Text label of the question to plot
         color_scheme (str): Color scheme of the plot
         display_title (bool): Whether to display the question title or not
         fig_dim (tuple of int): Dimensions of the plot. If none is given, default is (10, number of options * 2)
@@ -116,16 +27,12 @@ def create_bar_plot(
         wrap_text (bool): Whether to wrap text labels if they are too long for a single line
     """
 
-    # Create tick labels
-    tick_labels = [text_dict[column] for column in data_df.index]
-    if wrap_text:
-        tick_labels = ["\n".join(wrap(label, 20)) for label in tick_labels]
-
     # Create figure
     default_fontsize = 15
     bar_height = bar_thickness / (2 * bar_spacing)
     max_data = max(data_df["percentage"])
     min_data = min(data_df["percentage"])
+    total = data_df["counts"].sum()
     if fig_dim is None:
         fig_dim = (8, max(bar_spacing * data_df.shape[0] * 0.8, 8))
     fig = plt.figure(figsize=fig_dim)
@@ -160,15 +67,13 @@ def create_bar_plot(
         )
 
     # Display texts
-    wrapped_title = "\n".join(wrap(text_dict["question_text"], 45))
-    wrapped_bottom = "\n".join(wrap(text_dict["question_text"], 55))
+    wrapped_title = "\n".join(wrap(question_label, 45))
+    wrapped_bottom = "\n".join(wrap(question_label, 55))
     if display_title:
         plt.text(
-            x=-5, y=-1, s=wrapped_title, fontsize=default_fontsize * 1.2 / bar_spacing
+            x=0.5, y=-1, s=wrapped_title, fontsize=default_fontsize * 1.2 / bar_spacing
         )
-    bottom_text = (
-        f"""Total: {total_response}\n\'{wrapped_bottom}\' Relative response rates """
-    )
+    bottom_text = f"""Total: {total}\n\'{wrapped_bottom}\' Relative response rates """
     plt.title(bottom_text, fontsize=default_fontsize * 0.9 / bar_spacing, y=-0.1)
 
     plt.subplots_adjust(left=0.25, right=0.9, top=0.8, bottom=0.1)
@@ -178,72 +83,83 @@ def create_bar_plot(
     return fig
 
 
-def plot_multiple_choice_question(
-    path_to_data,
-    question_id,
-    path_to_structure=None,
+def make_tick_labels(data_df, question_choices, wrap_text=True):
+    """
+    Create tick labels
+
+    Args:
+        data_df (df): Dataframe of data to plot
+        question_chocies (dict): Dict mapping for answer choices
+        wrap_text (bool): Whether to wrap text labels if they are too long for a single line
+
+    Returns:
+        list: list of tick labels
+    """
+
+    tick_labels = [question_choices[column] for column in data_df.index]
+    if wrap_text:
+        tick_labels = ["\n".join(wrap(label, 20)) for label in tick_labels]
+
+    return tick_labels
+
+
+def filter_categories_below_threshold(data_df, display_threshold):
+    """Filter categories below threshold from dataframe
+
+    Args:
+        data_df (df): Dataframe of data to plot
+        display_threshold (float): Percentage threshold of the category to be included in the plot
+
+    Returns:
+        df: Dataframe with categories above display threshold only
+    """
+
+    return data_df[data_df["percentage"] > display_threshold]
+
+
+def make_bar_plot_for_multiple_choice_question(
+    data_df,
+    question_label,
+    question_choices,
     color_scheme="Blues_d",
     display_title=False,
     fig_dim=None,
     bar_thickness=1,
     bar_spacing=1,
-    display_no_answer=False,
     display_threshold=0,
     wrap_text=True,
     save_fig_file=False,
 ):
     """
-    Plots the response to a multiple choice question as a horizontal barplot
+    Plot the response to a multiple choice question as a horizontal barplot
 
     Args:
-        path_to_data (str): Path to raw data csv file
-        question_id (str): Code/id of the question to plot
-        path_to_structure (str): Path to survey structure xml file
+        data_df (df): dataframe of counts and percentages
+        question_label (str): Text label of the question to plot
+        question_choices (dict): dict mapping for answer chocies
         color_scheme (str): Color scheme of the plot
         display_title (bool): Whether to display the question title or not
         fig_dim (tuple of int): Dimensions of the plot. If none is given, default is (10, number of options * 2)
-        display_no_answer (bool): Whether to display the "No answer" category or not
         display_threshold (float): Percentage threshold of the category to be included in the plot
         wrap_text (bool): Whether to wrap text labels if they are too long for a single line
         save_fig_file (bool): Whether to save the figure to png file
     """
 
-    # Load raw data
-    data_df = load_raw_data(path_to_data, question_id)
+    filtered_data_df = filter_categories_below_threshold(data_df, display_threshold)
 
-    # Preprocess data
-    total_response, data_to_plot = preprocess_data(data_df, display_threshold)
-
-    # Extract question and response texts
-    if path_to_structure is not None:
-        text_dict = extract_question_info(path_to_structure, question_id)
-
-    # Create generic texts as substitute if real data unavailable
-    else:
-        text_dict = {
-            question_id
-            + "[SQ"
-            + str(i).zfill(3)
-            + "]": "Option "
-            + str(i)
-            + " text" * 10
-            for i in range(1, len(data_df.shape[1]) + 5)
-        }
-        text_dict[question_id + "[other]"] = "Other"
-        text_dict["question_text"] = f"Question {question_id} asks..."
+    tick_labels = make_tick_labels(filtered_data_df, question_choices, wrap_text)
 
     fig = create_bar_plot(
-        data_to_plot,
-        text_dict,
-        total_response,
+        filtered_data_df,
+        question_label,
+        tick_labels,
         color_scheme,
         display_title,
         fig_dim,
         bar_thickness,
         bar_spacing,
-        wrap_text,
     )
 
     # Save figure to file
     if save_fig_file:
-        fig.savefig(f"{question_id}_bar.png")
+        fig.savefig("multiple_choice_bar.png")
