7c6ad0ef5c96cdd276eb45059c38e5884ad02dda
schind65 <schind65@hzdr.de>
2022-01-12 20:42:57 +0100

    first form of combining plots stands. Now debugging

--- n2survey/plot/simple_comparison.py
+++ n2survey/plot/simple_comparison.py
@@ -4,12 +4,57 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import numpy as np
+import pandas as pd
 import seaborn as sns
 
 __all__ = ["simple_comparison_plot"]
 
-def form_xs_and_ys(data_dfs):
 
+def get_percentages(array):
+    (combi, 
+     no_of) = np.unique(array.astype(str), return_counts = True, axis=0)
+    # calculate percentages of x
+    percentage = {}
+    for answer in combi[:,0]:
+        answer_rows = np.where(combi[:,0] == answer)
+        percentage[answer] = np.append(
+            np.reshape(combi[answer_rows, 1], newshape = (-1,1)),
+            np.reshape(
+                np.round(no_of[answer_rows]/sum(no_of[answer_rows])*100, 
+                         decimals=1), newshape = (-1,1)
+                ), axis=1
+            )
+        # calculate barheights for overlapping bars
+        barheight = np.array([percentage[answer][0,1]]).astype(np.float64)
+        count = 0
+        for entry in percentage[answer][1:,1].astype(np.float64):
+            barheight = np.append(barheight,
+                                  barheight[count]+entry)
+            count = count+1
+        percentage[answer] = np.append(percentage[answer],
+                                       np.reshape(barheight, newshape=(-1, 1)),
+                                       axis=1)
+        percentage = pd.DataFrame.from_dict(percentage,
+            orient='index'
+                     )
+        return percentage
+    
+def form_xs_and_ys(data_dfs):
+    xs = []
+    ys = []
+    for row in data_dfs:
+        xrow = []
+        yrow = []
+        for entry in row:
+            if type(entry) == np.ndarray:
+                percentage = get_percentages(entry)
+                xrow.append(percentage.index.values)
+                yrow.append(pd.Series(percentage.iloc[:,0]))
+            else:
+                xrow.append(list(entry.index.values))
+                yrow.append(list(entry.iloc[:,0].values))
+        xs.append(xrow.copy())
+        ys.append(yrow.copy())
     return xs, ys
 
 def simple_comparison_plot(data_dfs,
@@ -22,6 +67,7 @@ def simple_comparison_plot(data_dfs,
                            hide_titles = False,
                            spacing_bar = False
                            ):
+    (xs,ys) = form_xs_and_ys(data_dfs)
     # %% additional parameters from bar.py
     # Additional parameters to provide in `sns.barplot`
     additional_params = {}
@@ -52,8 +98,9 @@ def simple_comparison_plot(data_dfs,
     # %% plotting
     
     for xrow,yrow in zip(xs,ys):
+        print(len(xs))
+        print(len(ys))
         for (x,y) in zip(xrow,yrow):
-            print(y)
             ax = fig.add_subplot(
                 len(xs), len(xrow), (xs.index(xrow)+1+xrow.index(x)*len(xs))
                 )
