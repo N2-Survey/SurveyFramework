5530394ff1e06acefadf9b791a66de30ae3995af
Dawei Fu <davidfuswe@gmail.com>
2022-01-14 15:54:34 +0100

    Implement rate_mental_health method for LimeSurvey class. Implement three test cases for rate_mental_health.

--- tests/test_lime_survey.py
+++ tests/test_lime_survey.py
@@ -515,6 +515,7 @@ class TestLimeSurveyGetChoices(BaseTestLimeSurvey2021Case):
         )
 
 
+<<<<<<< HEAD
 class TestLimeSurveyplot(BaseTestLimeSurvey2021WithResponsesCase):
     def test_single_choice_question(self):
         # Todo implement test using mocking or matplotlib.testing
@@ -534,6 +535,8 @@ class TestLimeSurveyplot(BaseTestLimeSurvey2021WithResponsesCase):
         )
 
 
+=======
+>>>>>>> Implement rate_mental_health method for LimeSurvey class. Implement three test cases for rate_mental_health.
 class TestLimeSurveyRateMentalHealth(BaseTestLimeSurvey2021WithResponsesCase):
     """Test LimeSurvey rate_mental_health"""
 
