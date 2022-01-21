161a31fa461f3cbebf32cb1fcbaf8a36a887c5f7
johnlee2104 <lcy9977@hotmail.com>
2022-01-21 18:07:22 +0100

    Check if pd.notna returns boolean or string

--- n2survey/lime/survey.py
+++ n2survey/lime/survey.py
@@ -259,9 +259,10 @@ class LimeSurvey:
             # ASSUME: question response consists of multiple columns with
             #         'Y' or NaN as entries.
             # Concatenate the last 'Other' column because it should not be masked with boolean values.
-            responses = pd.concat(
-                [pd.notna(responses.iloc[:, :-1]), responses.iloc[:, -1]], axis=1
-            )
+            # responses = pd.concat(
+            #     [pd.notna(responses.iloc[:, :-1]), responses.iloc[:, -1]], axis=1
+            # )
+            responses = pd.notna(responses)
 
         # replace labels
         if labels:
