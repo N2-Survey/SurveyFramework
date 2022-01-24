0a4f11521ca761b48e981408f42fa5ae8bd52d2b
Dawei Fu <davidfuswe@gmail.com>
2022-01-24 15:52:55 +0100

    Revert "Update rate_mental_health method and tests"
    This reverts commit 470255396cf592b7ebb36cf0902581a3c203ea97.

--- tests/test_lime_survey.py
+++ tests/test_lime_survey.py
@@ -561,7 +561,6 @@ class TestLimeSurveyRateMentalHealth(BaseTestLimeSurvey2021WithResponsesCase):
             index=[2, 3, 4],
         )
         ref.index.name = "id"
-        ref.columns.names = ["name"]
 
         self.assert_df_equal(result.iloc[:3, -2:], ref, msg="DataFrames not equal.")
 
@@ -586,7 +585,6 @@ class TestLimeSurveyRateMentalHealth(BaseTestLimeSurvey2021WithResponsesCase):
             index=[2, 3, 4],
         )
         ref.index.name = "id"
-        ref.columns.names = ["name"]
 
         self.assert_df_equal(result.iloc[:3, -2:], ref, msg="DataFrames not equal.")
 
@@ -613,7 +611,6 @@ class TestLimeSurveyRateMentalHealth(BaseTestLimeSurvey2021WithResponsesCase):
             index=[2, 3, 4],
         )
         ref.index.name = "id"
-        ref.columns.names = ["name"]
 
         self.assert_df_equal(result.iloc[:3, -2:], ref, msg="DataFrames not equal.")
 
