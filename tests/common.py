68d22293a7dcba6fbc191bac4f6e511c4d0fabe1
Dawei Fu <davidfuswe@gmail.com>
2022-02-25 17:44:41 +0100

    Fix __getitem__ method

--- tests/common.py
+++ tests/common.py
@@ -43,8 +43,7 @@ class BaseTestLimeSurvey2021Case(BaseTestCase):
         cls.array_column = "B6"
         cls.free_column = "A8"
         # Read lime survey
-        cls.survey = LimeSurvey()
-        cls.survey.read_structure(structure_file=cls.structure_file)
+        cls.survey = LimeSurvey(structure_file=cls.structure_file)
 
 
 class BaseTestLimeSurvey2021WithResponsesCase(BaseTestLimeSurvey2021Case):
