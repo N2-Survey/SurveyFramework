316f38c7eac4ad672d5baf6d87f35419abd67483
Dawei Fu <davidfuswe@gmail.com>
2022-02-25 17:44:41 +0100

    Fix __getitem__ method

--- tests/test_lime_survey.py
+++ tests/test_lime_survey.py
@@ -19,8 +19,7 @@ class TestLimeSurveyInitialisation(BaseTestLimeSurvey2021Case):
     def test_simple_init(self):
         """Test simple initialisation"""
 
-        survey = LimeSurvey()
-        survey.read_structure(structure_file=self.structure_file)
+        survey = LimeSurvey(structure_file=self.structure_file)
 
         structure_dict = read_lime_questionnaire_structure(self.structure_file)
         section_df = pd.DataFrame(structure_dict["sections"])
@@ -42,7 +41,6 @@ class TestLimeSurveyInitialisation(BaseTestLimeSurvey2021Case):
             output_folder="somefolder",
             structure_file=self.structure_file,
         )
-        survey.read_structure(structure_file=self.structure_file)
         expected_theme = DEFAULT_THEME.copy()
         expected_theme["palette"] = "Reds"
         self.assertEqual(survey.theme, expected_theme)
