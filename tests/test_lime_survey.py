af1d9f51e440735a1cdfdf3f66e7b1a37b4751a6
NiRuff <n.ruffini@gmx.de>
2022-06-14 11:14:43 +0200

    adjusted test to new plot_title parameter

--- tests/test_lime_survey.py
+++ tests/test_lime_survey.py
@@ -1007,7 +1007,7 @@ class TestLimeSurveyplot(BaseTestLimeSurvey2021WithResponsesCase):
         self.survey.plot(
             self.multiple_choice_column,
             rc={"font.sans-serif": "Tahoma"},
-            display_title=True,
+            plot_title=True,
         )
 
         self.survey.plot(
