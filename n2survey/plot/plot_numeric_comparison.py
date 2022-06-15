3e8a68f8266aff56b9277afb22f8ba0f79cc841b
j-pietsch <julianpietsch1@gmail.com>
2022-06-15 13:34:08 +0200

    Sourced out individual subplot-plotting, added more comments. Setting up different theme works now.

--- n2survey/plot/plot_numeric_comparison.py
+++ n2survey/plot/plot_numeric_comparison.py
@@ -1,5 +1,5 @@
 from textwrap import wrap
-from typing import Dict, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple
 
 import matplotlib as mpl
 import matplotlib.patheffects as pe
@@ -69,143 +69,202 @@ def get_median_and_range(survey, question, responses):
         return np.nan, np.nan
 
 
+def plot_subplot_barplot(
+    ax,
+    x,
+    y,
+    survey,
+    question,
+    responses,
+    labels,
+    color,
+    display_total,
+    display_percents,
+    display_median,
+    percent_threshold,
+    y_axis_max,
+    **additional_params,
+):
+    """Plot a single barplot.
+
+    Args:
+        ax (mpl.axes.Axes): Axes used for the current subplot.
+        x (list): List of indices used as x-axis
+        y (pandas Series): Counts of the respective bins.
+        survey (LimeSurvey object): LimeSurvey object with all transformed questions.
+        question (str): Investigated question, used for selecting data to calculate median.
+        responses (pandas DataFrame): DataFrame with all (uncounted) responses.
+        labels (str): Current labels for the `responses`, plotted at right top of axes.
+
+        display_percents (bool): Show percents on top of bars. Defaults to False.
+        display_total (bool): Show total number on the figure. Defaults to True.
+        display_median (bool): Show median of distribution. Defaults to True.
+        percent_threshold (int): Threshold for displaying percentages over bars.
+        y_axis_max (int): Maximum of y-axis, array-like or the same value for all if int.
+
+
+    Returns:
+        ax (mpl.axes.Axes): Updated axes of the plot
+    """
+    sns.barplot(x=x, y=y, ci=None, ax=ax, color=color, **additional_params)
+    ax.set(ylabel=None)
+    total = np.array(y).sum()
+    if display_total:
+        ax.text(
+            1.0,
+            1.05,
+            f"{labels}: {total}",
+            c=color,
+            horizontalalignment="right",
+            verticalalignment="center",
+            transform=ax.transAxes,
+        )
+    # Add percents on top
+    if display_percents:
+        percents = 100 * np.array(y) / total
+        labels = [
+            "{:.1f}%".format(p) if p > percent_threshold else "" for p in percents
+        ]
+        ax.bar_label(ax.containers[0], labels)
+    # Add median as vertical dashed line
+    if display_median:
+        median, converted_range = get_median_and_range(survey, question, responses)
+        if converted_range in np.asarray(x):
+            plot_position = np.where(np.asarray(x) == converted_range)[0]
+            ax.axvline(x=plot_position, ls="--", c="grey")
+            ax.text(
+                plot_position + 0.1,
+                1.20 * np.max(y),
+                f"{converted_range}",
+                c="white",
+                weight="bold",
+                path_effects=[pe.withStroke(linewidth=2, foreground="black")],
+            )
+    # Adjust yscale manually to show highest percentage values
+    if y_axis_max is not None:
+        ax.set_ylim(0, y_axis_max)
+    else:
+        ax.set_ylim(0, 1.40 * np.max(y))
+    ax.yaxis.get_major_locator().set_params(integer=True)
+    return ax
+
+
 def comparison_numeric_bar_plot(
     survey,
     question,
     list_of_responses,
-    list_of_data,
-    list_of_labels: list = None,
-    total: Union[int, float, None] = None,
-    title: Optional[str] = None,
+    list_of_counts,
+    list_of_labels,
+    title: str = None,
     fig_size_inches: tuple = None,
+    theme: Optional[Dict] = None,
     display_percents: bool = False,
     display_total: bool = True,
     display_median: bool = True,
     display_no_answer: bool = True,
-    wrap_text=True,
-    percent_threshold=5,
-    yaxis_max=None,
-    theme: Optional[Dict] = None,
+    wrap_text: bool = True,
+    percent_threshold: int = 5,
+    y_axis_max: bool = None,
 ) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
-    """Do a multiple bar plots for a numeric single-choice question with multiple filters.
+    """Do multiple bar plots for a numeric single-choice question with multiple filters.
 
     Args:
-        question (str): investigated question, used for selecting data to calculate median.
-        list_of_responses (list): unfiltered and filtered data to plot.
-        list_of_data (list): unfiltered and filtered counted data to plot.
-        list_of_labels (list): labels for the entries in `list_of_data`.
-        total (Union[int, float, None], optional): Total number to calculate percentage
-          and to show on the figure. By default, sum of Y values is used.
-        title (Optional[str], optional): Title of the axes. Defaults to None.
-        display_percents (bool, optional): Show percents on top of boxes. Defaults to False.
+        survey (LimeSurvey object): LimeSurvey object with all transformed questions.
+        question (str): Investigated question, used for selecting data to calculate median.
+        list_of_responses (list): Unfiltered and filtered data.
+        list_of_counts (list): Unfiltered and filtered counted data to plot.
+        list_of_labels (list): Labels for the entries in `list_of_counts`, plotted at right top of axes.
+        title (str, optional): Title of the uppermost axes. Defaults to None.
+        fig_size_inches (tuple, optional): Size of the resulting Figure. Defaults to None.
+        theme (Optional[Dict], optional): Seaborn theme parameters.
+          See `seaborn.set_theme` for the details. Defaults to None.
+        display_percents (bool, optional): Show percents on top of bars. Defaults to False.
         display_total (bool, optional): Show total number on the figure. Defaults to True.
         display_median (bool, optional): Show median of distribution. Defaults to True.
-        display_no_answer (bool, optional): show 'No answer' possiblities. Defaults to True.
+        display_no_answer (bool, optional): Show 'No answer' possiblities. Defaults to True.
         wrap_text (bool, optional): Add line breaks to long questions. Defaults to True.
         percent_threshold (int, optional): Threshold for displaying percentages over bars.
         y_axis_max (): Maximum of y-axis, array-like or the same value for all if int.
-        theme (Optional[Dict], optional): seaborn theme parameters.
-          See `seaborn.set_theme` for the details. Defaults to None.
 
     Returns:
-        tuple[mpl.figure.Figure, mpl.axes.Axes]: tuple (fig, ax) of the plot
+        tuple[mpl.figure.Figure, mpl.axes.Axes]: Tuple (fig, ax) of the plot
     """
     # Additional parameters to provide in `sns.barplot`
     additional_params = {}
 
     # Elements of color `palette` are used for individual subplots
-    palette = None
     if theme is not None:
         sns.set_theme(**theme)
-        palette = theme.get("palette", None)
+        palette_cmap = sns.color_palette(theme.get("palette", None), as_cmap=True)
     else:
-        palette = sns.color_palette()
+        palette_cmap = sns.color_palette(as_cmap=True)
+    # Get a list of colors in line with palette (some palettes are almost white at edges
+    # Therefore, do not go from 0-1, but from 0.2-0.8
+    list_colors = [palette_cmap(i) for i in np.linspace(0.2, 0.8, len(list_of_counts))]
 
     # Comparison only done in one figure (max. 9 subplots)
-    if len(list_of_data) > 9:
+    if len(list_of_counts) > 9:
         raise NotImplementedError(
             "Implementation only supports a maximum of 8 comparisons!"
         )
 
-    # set figure size according to the number of subplots and x-axis entries
+    # Set figure size according to the number of subplots and x-axis entries
     if fig_size_inches is None:
-        fig_size_inches = (len(list_of_data[0]) / 2.0, 2.0 * len(list_of_data))
+        fig_size_inches = (len(list_of_counts[0]) / 1.5, 2.0 * len(list_of_counts))
 
-    # filter out all 'No answer' entries for all dataframes
+    # Filter out all 'No answer' entries for all dataframes
     if not display_no_answer:
         try:
-            for i in range(len(list_of_data)):
-                list_of_data[i] = list_of_data[i].drop("No Answer")
+            for i in range(len(list_of_counts)):
+                list_of_counts[i] = list_of_counts[i].drop("No Answer")
         except KeyError:
             pass
 
     # Prepare figure:
     # Total at top, all other filtered data below
-    fig, ax = plt.subplots(len(list_of_data), 1, sharex=True)
+    fig, ax = plt.subplots(len(list_of_counts), 1, sharex=True)
     fig.set_size_inches(fig_size_inches)
     fig_width, _ = fig.get_size_inches()
 
-    if yaxis_max is not None:
-        if isinstance(yaxis_max, int):
-            yaxis_max = np.ones(len(list_of_data)) * yaxis_max
+    if y_axis_max is not None:
+        if isinstance(y_axis_max, int):
+            y_axis_max = np.ones(len(list_of_counts)) * y_axis_max
         else:
-            if len(np.asarray(yaxis_max)) == len(list_of_data):
+            if len(np.asarray(y_axis_max)) == len(list_of_counts):
                 pass
-    # Do the plots: unfiltered at top, then all plots with filtered data
-    for i in range(len(list_of_data)):
-        data = list_of_data[i]
+    # Do all subplots: unfiltered at top, then all subplots with filtered data
+    for i in range(len(list_of_counts)):
+        data = list_of_counts[i]
         x = data.iloc[:, 0].index.values.tolist()
         y = data.iloc[:, 0]
-        sns.barplot(x=x, y=y, ci=None, ax=ax[i], color=palette[i], **additional_params)
-        ax[i].set(ylabel=None)
-        total = np.array(y).sum()
-        if display_total:
-            ax[i].text(
-                1.0,
-                1.05,
-                f"{list_of_labels[i]}: {total}",
-                c=palette[i],
-                horizontalalignment="right",
-                verticalalignment="center",
-                transform=ax[i].transAxes,
-            )
-        # Add percents on top
-        if display_percents:
-            percents = 100 * np.array(y) / total
-            labels = [
-                "{:.1f}%".format(p) if p > percent_threshold else "" for p in percents
-            ]
-            ax[i].bar_label(ax[i].containers[0], labels)
-        # Add median as vertical dashed line
-        if display_median:
-            median, converted_range = get_median_and_range(
-                survey, question, list_of_responses[i]
-            )
-            if converted_range in np.asarray(x):
-                plot_position = np.where(np.asarray(x) == converted_range)[0]
-                ax[i].axvline(x=plot_position, ls="--", c="grey")
-                ax[i].text(
-                    plot_position,
-                    1.20 * np.max(y),
-                    f"{converted_range}",
-                    c="white",
-                    weight="bold",
-                    path_effects=[pe.withStroke(linewidth=2, foreground="black")],
-                )
-        # Adjust yscale manually to show highest percentage values
-        if yaxis_max is not None:
-            ax[i].set_ylim(0, yaxis_max[i])
-        else:
-            ax[i].set_ylim(0, 1.40 * np.max(y))
-        ax[i].yaxis.get_major_locator().set_params(integer=True)
-
+        ax[i] = plot_subplot_barplot(
+            ax[i],
+            x,
+            y,
+            survey,
+            question,
+            list_of_responses[i],
+            list_of_labels[i],
+            list_colors[i],
+            display_total,
+            display_percents,
+            display_median,
+            percent_threshold,
+            y_axis_max,
+            **additional_params,
+        )
+    # Adjustments for the whole figure:
     # Set y-label only for the axis (roughly) in the middle
-    ax[int(len(list_of_data) / 2)].set_ylabel("Number of Responses")
+    ax[int(len(list_of_counts) / 2)].set_ylabel("Number of Responses")
+
+    if wrap_text:
+        title = "\n".join(wrap(title, 55))
+        tick_labels = ["\n".join(wrap(label, 20)) for label in x]
+        ax[-1].set_xticklabels(tick_labels)
 
     # Set title for uppermost axes
     if title is not None:
-        wrapped_title = "\n".join(wrap(title, 55))
-        ax[0].set_title(wrapped_title, pad=20)
+        ax[0].set_title(title, pad=20)
 
     # Figure settings
     fig.tight_layout()
